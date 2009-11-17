dd76393f1c7d3aef0251e143545729bbcb071f5e
Cezary Bartoszuk <cbart@students.mimuw.edu.pl>
2009-11-18 00:18:07 +0100

    Now using debug_tools in quatsec.cc

--- quaternion/quatsec.cc
+++ quaternion/quatsec.cc
@@ -2,18 +2,23 @@
 #include <stdexcept>
 #include <map>
 
+#include "./debug_tools.h"
 #include "./quatsec.h"
 #include "./quaternion.h"
 
 #ifdef DEBUGLEVEL
-    const int DEBUG_LEVEL = DEBUGLEVEL;
+    const DebugLevel DEBUG_LEVEL = DEBUGLEVEL;
 #else
-    const int DEBUG_LEVEL = 0;
+    const DebugLevel DEBUG_LEVEL = 0;
 #endif
 
 typedef QuaternionSequence::ProxyQContainer ProxyQContainer;
-
 typedef QuaternionSequence::size_type size_type;
+
+const String DIAG_PROG_NAME = "Quatsec";
+const DebugLevel ERROR_DEBUG_LEVEL = 2;
+const DebugLevel WARNING_DEBUG_LEVEL = 2;
+const DebugLevel INFO_DEBUG_LEVEL = 2;
 
 ProxyQContainer::ProxyQContainer
     (QuaternionSequence::size_type id,
@@ -21,26 +26,17 @@ ProxyQContainer::ProxyQContainer
   : index(id),
     qs(&qs)
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequece::"
-                     "ProxyQContainer::"
-                     "ProxyQContainer"
-                     "(QuaternionSequence::size_type id);\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequece::ProxyQContainer::"
+        "ProxyQContainer(QuaternionSequence::size_type id);\n" << std::flush;
 }
 
 ProxyQContainer::ProxyQContainer(const QuaternionSequence::ProxyQContainer& cnt)
   : index(cnt.index),
     qs(cnt.qs)
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::"
-                     "ProxyQContainer"
-                     "(QuaternionSequence::ProxyQContainer& cnt);\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::ProxyQContainer::"
+        "ProxyQContainer(QuaternionSequence::ProxyQContainer& cnt);\n"
+        << std::flush;
 }
 
 ProxyQContainer& ProxyQContainer::operator=
@@ -53,31 +49,19 @@ ProxyQContainer& ProxyQContainer::operator=
 
 ProxyQContainer::~ProxyQContainer()
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequece::"
-                     "ProxyQContainer::"
-                     "~ProxyQContainer();\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequece::ProxyQContainer::~ProxyQContainer();\n"
+        << std::flush;
 }
 
 ProxyQContainer* ProxyQContainer::clone() const
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::clone();\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::ProxyQContainer::clone();\n"
+        << std::flush;
     return new ProxyQContainer(index, *qs);
 }
 
 Real ProxyQContainer::getA() const
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::getA() const;\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter != qs->seq.end())
@@ -88,11 +72,6 @@ Real ProxyQContainer::getA() const
 
 Real ProxyQContainer::getB() const
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::getB() const;\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter != qs->seq.end())
@@ -103,11 +82,6 @@ Real ProxyQContainer::getB() const
 
 Real ProxyQContainer::getC() const
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::getC() const;\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter != qs->seq.end())
@@ -118,11 +92,6 @@ Real ProxyQContainer::getC() const
 
 Real ProxyQContainer::getD() const
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::getD() const;\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter != qs->seq.end())
@@ -133,74 +102,40 @@ Real ProxyQContainer::getD() const
 
 void ProxyQContainer::setA(Real a)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::setA(Real a);\n"
-                     "\tCalled with: " << a << ".\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter == qs->seq.end())
         qs->seq[index] = Quaternion();
     qs->seq[index].val->setA(a);
-
 }
 
 void ProxyQContainer::setB(Real b)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::setB(Real b);\n"
-                     "\tCalled with: " << b << ".\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter == qs->seq.end())
         qs->seq[index] = Quaternion();
     qs->seq[index].val->setB(b);
-
 }
 
 void ProxyQContainer::setC(Real c)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::setC(Real c);\n"
-                     "\tCalled with: " << c << ".\n"
-                  << std::flush;
     std::map<size_type, Quaternion>::iterator iter;
     iter = qs->seq.find(index);
     if(iter == qs->seq.end())
         qs->seq[index] = Quaternion();
     qs->seq[index].val->setC(c);
-
 }
 
 void ProxyQContainer::setD(Real d)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::"
-                     "ProxyQContainer::setD(Real d);\n"
-                     "\tCalled with: " << d << ".\n"
-                  << std::flush;
     if(qs->seq.find(index) == qs->seq.end())
         qs->seq[index] = Quaternion();
     qs->seq[index].val->setD(d);
-
 }
 
 Quaternion& QuaternionSequence::getQuaternion(size_type id)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::getQuaternion"
-                     "(size_type id);\n"
-                     "\tCalled with: " << id << ".\n"
-                  << std::flush;
     if(seq.find(id) == seq.end())
         seq[id] = Quaternion();
     return seq[id];
@@ -209,13 +144,10 @@ Quaternion& QuaternionSequence::getQuaternion(size_type id)
 Quaternion& QuaternionSequence::setQuaternion
     (size_type id, const Quaternion& q)
 {
-    if(DEBUG_LEVEL > 4)
-        std::cerr << "(II) "
-                     "QuaternionSequence::setQuaternion"
-                     "(size_type id, Quaternion q);\n"
-                     "\tCalled with (id): " << id << ",\n"
-                     "\tCalled with (q):  " << q << ".\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::setQuaternion"
+        "(size_type id, Quaternion q);\n"
+        "\tCalled with (id): " << id << ",\n"
+        "\tCalled with (q):  " << q << ".\n" << std::flush;
     seq[id] = Quaternion(q);
     return seq[id];
 }
@@ -223,33 +155,25 @@ Quaternion& QuaternionSequence::setQuaternion
 QuaternionSequence::QuaternionSequence()
   : seq(std::map<size_type, Quaternion>())
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::QuaternionSequence();\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::QuaternionSequence();\n"
+        << std::flush;
 }
 
 QuaternionSequence::QuaternionSequence
     (const std::map<size_type, Quaternion>& m_seq)
   : seq(m_seq)
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::QuaternionSequence"
-                     "(const std::map<QuaternionSequence::size_type, "
-                     "Quaternion>);\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::QuaternionSequence"
+        "(const std::map<QuaternionSequence::size_type, Quaternion>);\n"
+        << std::flush;
 }
 
 QuaternionSequence::QuaternionSequence
     (const std::vector<Quaternion>& v_seq)
   : seq(std::map<size_type, Quaternion>())
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::QuaternionSequence"
-                     "(const std::vector<Quaternion>& v_seq);\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::QuaternionSequence"
+        "(const std::vector<Quaternion>& v_seq);\n" << std::flush;
     size_type i = 0;
     for(std::vector<Quaternion>::const_iterator iter = v_seq.begin();
             iter < v_seq.end();
@@ -261,19 +185,13 @@ QuaternionSequence::QuaternionSequence
     (const QuaternionSequence& qs)
   : seq(std::map<size_type, Quaternion>(qs.seq))
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::QuaternionSequence"
-                     "(const QuaternionSequence& qs);\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::QuaternionSequence"
+        "(const QuaternionSequence& qs);\n" << std::flush;
 }
 
 QuaternionSequence::~QuaternionSequence()
 {
-    if(DEBUG_LEVEL > 3)
-        std::cerr << "(II) "
-                     "QuaternionSequence::~QuaternionSequence();\n"
-                  << std::flush;
+    logInfo() << "QuaternionSequence::~QuaternionSequence();" << std::flush;
 }
 
 
